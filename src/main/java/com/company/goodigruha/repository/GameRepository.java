fcd708166409835b5009716d3b05d3b2710a2526
Vitaliy <v.babich.225@gmail.com>
2023-02-06 12:36:42 +0200

    Refactoring

--- src/main/java/com/company/goodigruha/repository/GameRepository.java
+++ src/main/java/com/company/goodigruha/repository/GameRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     @Query("SELECT g FROM Game g")
-    Optional<Game> getAll();
+    List<Game> getAll();
 }
 
 
